8ecccb70549b1171a367937fe402f812a3475786
tom <1395039366@qq.com>
2018-11-30 15:48:55 +0800

    组件拆分

---
+++ src/components/breadcrumb.vue
@@ -0,0 +1,31 @@
+<style lang="less" scoped type="text/less">
+
+</style>
+
+<template>
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">Home</a>
+        <slot name="bread">
+
+        </slot>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+	export default {
+		props: [],
+		data() {
+			return {}
+		},
+		components: {},
+		computed: {},
+		methods: {},
+		mounted() {
+
+		}
+	}
+</script>
---
+++ src/components/footer.vue
@@ -0,0 +1,49 @@
+<style lang="less" scoped type="text/less">
+
+</style>
+
+<template>
+  <footer class="footer">
+    <div class="footer__wrap">
+      <div class="footer__secondary">
+        <div class="footer__inner">
+          <div class="footer__region">
+            <span>Region</span>
+            <select class="footer__region__select">
+              <option value="en-US">USA</option>
+              <option value="zh-CN">China</option>
+              <option value="in">India</option>
+            </select>
+          </div>
+          <div class="footer__secondary__nav">
+            <span>Copyright © 2017 IMooc All Rights Reserved.</span>
+            <a href="http://us.lemall.com/us/aboutUs.html">
+              About Us
+            </a>
+            <a href="http://us.lemall.com/us/termsofUse.html">
+              Terms &amp; Conditions
+            </a>
+            <a href="http://us.lemall.com/us/privacyPolicy.html">
+              Privacy Policy
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+    props: [],
+    data() {
+      return {}
+    },
+    components: {},
+    computed: {},
+    methods: {},
+    mounted() {
+
+    }
+  }
+</script>
